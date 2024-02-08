b524dc9d61cfe8bca037c0ad052572d1fc196826
VGarlapati2027 <85514686+VGarlapati2027@users.noreply.github.com>
2024-02-07 18:03:03 -0800

    Add files via upload

---
+++ PRO-104-Project-Image-main/pic.py
@@ -0,0 +1,14 @@
+import cv2
+
+image = cv2.imread = ('PRO-104-Project-Image-main/solar-system.jpg')
+
+
+cv2.putText(image,'Sun',(50,50),fontFace=cv2.FONT_HERSHEY_DUPLEX,fontScale=0.75,color=(255,255,255))
+cv2.putText(image,'Mercury',(50,50),fontFace=cv2.FONT_HERSHEY_DUPLEX,fontScale=0.75,color=(255,355,255))
+cv2.putText(image,'Venus',(50,50),fontFace=cv2.FONT_HERSHEY_DUPLEX,fontScale=0.75,color=(255,455,255))
+cv2.putText(image,'Earth',(50,50),fontFace=cv2.FONT_HERSHEY_DUPLEX,fontScale=0.75,color=(255,555,255))
+cv2.putText(image,'Mars',(50,50),fontFace=cv2.FONT_HERSHEY_DUPLEX,fontScale=0.75,color=(255,655,255))
+cv2.putText(image,'Jupiter',(50,50),fontFace=cv2.FONT_HERSHEY_DUPLEX,fontScale=0.75,color=(255,755,255))
+cv2.putText(image,'Saturn',(50,50),fontFace=cv2.FONT_HERSHEY_DUPLEX,fontScale=0.75,color=(255,855,255))
+cv2.putText(image,'Uranus',(50,50),fontFace=cv2.FONT_HERSHEY_DUPLEX,fontScale=0.75,color=(255,955,255))
+cv2.putText(image,'Neptune',(50,50),fontFace=cv2.FONT_HERSHEY_DUPLEX,fontScale=0.75,color=(255,1055,255))
